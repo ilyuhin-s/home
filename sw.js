2b92b705346863b384ada1c00b8285587e8b4d1c
Sergey Ilyuhin <ilyuhin-s@mail.ru>
2025-09-26 01:10:54 +0500

    add

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = "my-pwa-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
+  "/home/",
+  "/home/index.html",
+  "/home/style.css",
+  "/home/icons/icon-192.png",
+  "/home/icons/icon-512.png",
 ];
 
 // Установка кэша при установке SW
