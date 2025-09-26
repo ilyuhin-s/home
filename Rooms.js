a95576ac8700cd21e8f2ce4f16309e5997627a0a
Sergey Ilyuhin <ilyuhin-s@mail.ru>
2025-09-26 22:18:50 +0500

    add

--- Rooms.js
+++ Rooms.js
@@ -1,7 +1,16 @@
 function Rooms() {
   return (
     <div className="section">
-      <div className="section-title">Rooms</div>
+      <div className="rooms">
+        <div className="rooms-title">Rooms</div>
+        <div className="room-items">
+          <div className="rooms-item rooms-item--active">Спальня</div>
+          <div className="rooms-item">Кухня</div>
+          <div className="rooms-item">Гостинная</div>
+          <div className="rooms-item">Ванная</div>
+          <div className="rooms-item">Коридор</div>
+        </div>
+      </div>
     </div>
   );
 }
