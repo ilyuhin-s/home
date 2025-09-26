de4fbc29e0b2a50c82819b59ba6ed441bd89835b
Sergey Ilyuhin <ilyuhin-s@mail.ru>
2025-09-26 23:00:54 +0500

    add

--- Rooms.js
+++ Rooms.js
@@ -4,11 +4,26 @@ function Rooms() {
       <div className="rooms">
         <div className="rooms-title">Rooms</div>
         <div className="room-items">
-          <div className="rooms-item rooms-item--active">Спальня</div>
-          <div className="rooms-item">Кухня</div>
-          <div className="rooms-item">Гостинная</div>
-          <div className="rooms-item">Ванная</div>
-          <div className="rooms-item">Коридор</div>
+          <div className="rooms-item rooms-item--active">
+            Спальня
+            <span className="material-icons">king_bed</span>
+          </div>
+          <div className="rooms-item">
+            Кухня
+            <span className="material-icons">restaurant</span>
+          </div>
+          <div className="rooms-item">
+            Гостинная
+            <span className="material-icons">weekend</span>
+          </div>
+          <div className="rooms-item">
+            Ванная
+            <span className="material-icons">clean_hands</span>
+          </div>
+          <div className="rooms-item">
+            Коридор
+            <span className="material-icons">door_front</span>
+          </div>
         </div>
       </div>
     </div>
