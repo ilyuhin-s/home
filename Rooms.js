f99a4c4ceba8af3b606feae1906c44537d4ef1e7
Sergey Ilyuhin <ilyuhin-s@mail.ru>
2025-09-28 14:46:33 +0500

    add

--- Rooms.js
+++ Rooms.js
@@ -1,20 +1,28 @@
 function Rooms() {
   return (
     <div className="section">
+      <div className="groups">
+        <div className="group group--active">All</div>
+        <div className="group">Bedroom</div>
+        <div className="group">Kitchen</div>
+        <div className="group">Living room</div>
+        {/* <div className="group">Bathroom</div> */}
+        {/* <div className="group">Hallway</div> */}
+      </div>
       <div className="rooms">
-        <div className="rooms-title">Rooms</div>
+        <div className="rooms-title">Devices</div>
         <div className="room-items">
           <div className="rooms-item rooms-item--active">
-            <span className="material-icons">king_bed</span>
-            Bedroom
+            <span className="material-icons">wb_incandescent</span>
+            Spotlights
           </div>
           <div className="rooms-item">
-            <span className="material-icons">restaurant</span>
-            Kitchen
+            <span className="material-icons">wb_iridescent</span>
+            LED strip
           </div>
           <div className="rooms-item">
-            <span className="material-icons">weekend</span>
-            Living room
+            <span className="material-icons">ac_unit</span>
+            Air conditioner
           </div>
           <div className="rooms-item">
             <span className="material-icons">clean_hands</span>
